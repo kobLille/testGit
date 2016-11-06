5f500a91e3ed59be1542f6027641ed9bf891f228
ste <ste@stePort>
2016-11-06 07:31:07 +0100

    modif

--- src/testGit/main.java
+++ src/testGit/main.java
@@ -4,8 +4,9 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-ecrire msg1 = new ecrire("coucou");
+ecrire msg1 = new ecrire("coucou1");
 ecrire msg2 = new ecrire("coucou2");
+ecrire msg3 = new ecrire("coucou3");
 	}
 
 }
